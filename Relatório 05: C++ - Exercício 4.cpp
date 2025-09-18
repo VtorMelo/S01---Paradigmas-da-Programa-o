cfd7752885122b95e8d18b9869b21d7ef7dbf267
Vitor Augusto Teixeira de Melo <vitoratmelo@gmail.com>
2025-09-18 20:52:00 -0300

    Create Relatório 05: C++ - Exercício 4.cpp

---
+++ Relatório 05: C++ - Exercício 4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+double celsiusParaFahrenheit(double celsius)
+{
+    return (celsius * 9.0 / 5.0) + 32.0;
+}
+
+double celsiusParaKelvin(double celsius)
+{
+    return celsius + 273.15;
+}
+
+int main() 
+{
+    int opcao;
+    double celsius;
+
+    do 
+    {
+        cout << "Menu de conversão" << endl;
+        cout << "1. Converter Celsius para Fahrenheit" << endl;
+        cout << "2. Converter Celsius para Kelvin" << endl;
+        cout << "3. Sair" << endl;
+        cout << "Escolha uma opcao: ";
+        cin >> opcao;
+
+        switch (opcao) 
+        {
+            case 1:
+                cout << "Digite a temperatura em Celsius: ";
+                cin >> celsius;
+                cout << "Em Fahrenheit: " << celsiusParaFahrenheit(celsius) << " °F" << endl;
+                break;
+
+            case 2:
+                cout << "Digite a temperatura em Celsius: ";
+                cin >> celsius;
+                cout << "Em Kelvin: " << celsiusParaKelvin(celsius) << " K" << endl;
+                break;
+
+            case 3:
+                cout << "Encerrando o programa..." << endl;
+                break;
+
+            default:
+                cout << "Opcao invalida! Tente novamente." << endl;
+        }
+
+    } while (opcao != 3);
+
+    return 0;
+}
